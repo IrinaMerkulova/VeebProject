eb0bedb290dd6c69909d218426dc4d3e4b51c76a
Kirill Bochkovoy <kirill.bochkovoy.rr4@gmail.com>
2018-05-21 11:07:59 +0300

    current time added

---
+++ opilased.php
@@ -0,0 +1,44 @@
+﻿<!DOCTYPE HTML>
+<html>
+<head>
+<title>Kana kool</title>
+	<?php
+	echo "The time is " . date("h:ia");
+	?>
+<link rel="stylesheet" type="text/css" href="kukestyle.css">
+</head>
+<body>
+<h1>Kuke KOOL</h1>
+<div id="logo">
+<img src="petuh.png">
+</div>
+<div id="tabel">
+<table>
+
+	<tr>
+	<td><a href="uudised.html">Uudised</a></td>
+	<td><a href="kontakt.html">Kontakt</a></td>
+	<td><a href="tunniplaan.html">Tunniplaan</a></td>
+	<td><a href="opilased.html">Õpilased</a></td>
+	<td><a href="opetajad.html">Õpetajad</a></td>
+	</tr>
+
+	<tr>
+	<td colspan="5" class="suur">
+	<ul>
+		<li><b>Õpilaste nimekiri:</b>
+		<li>Kukene
+		<li>Kanakene
+		<li>Kana Suur
+		<li>Kana Väike
+		<li>Kuri Kukk
+	</ul>
+	</td>
+	</tr>
+
+</table>
+</div>
+<a><img src="http://i.imgur.com/rU0I2.png"></a>
+<a><img src="http://i.imgur.com/rU0I2.png"></a>
+</body>
+</html>
---
+++ tunniplaan.php
@@ -0,0 +1,35 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Kana kool</title>
+	<?php
+	echo "The time is " . date("h:ia");
+	?>
+<link rel="stylesheet" type="text/css" href="kukestyle.css">
+</head>
+<body>
+<h1>Kuke KOOL</h1>
+<div id="logo">
+<img src="petuh.png">
+</div>
+<div id="tabel">
+<table>
+	<tr>
+	<td><a href="uudised.html">Uudised</a></td>
+	<td><a href="kontakt.html">Kontakt</a></td>
+	<td><a href="tunniplaan.html">Tunniplaan</a></td>
+	<td><a href="opilased.html">Õpilased</a></td>
+	<td><a href="opetajad.html">Õpetajad</a></td>
+	</tr>
+	<tr>
+	<td colspan="5" class="suur">
+    <iframe width="560" height="315" src="http://www.youtube.com/embed/IjbQnY4j4cw" frameborder="0" allowfullscreen></iframe>
+	</td>
+	</tr>
+
+</table>
+</div>
+<a><img src="http://i.imgur.com/rU0I2.png"></a>
+<a><img src="http://i.imgur.com/rU0I2.png"></a>
+</body>
+</html>
