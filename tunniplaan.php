297344b79ca5e0feca43a23aa16d00cd9800c76f
nekrasov <ilia.n3kr4s0v@gmail.com>
2018-05-21 11:01:16 +0300

    fix

---
+++ tunniplaan.php
@@ -0,0 +1,54 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title>Kana kool</title>
+<link rel="stylesheet" type="text/css" href="kukestyle.css">
+<!--stili oformlenija iz faila kukestyle.css-->
+</head>
+<body>
+<h1>Kuke KOOL</h1>
+<?php
+$dt = new DateTime();
+echo $dt->format('H:i');
+?>
+<!-- razdel s id= logo-->
+<div id="logo">
+<img src="petuh.png">
+</div>
+<!-- razdel s id= tabel-->
+<div id="tabel">
+<table>
+<!--1.stroka--> 
+	<!-- <tr>
+	<td><a href="uudised.html">Uudised</a></td>
+	<td><a href="kontakt.html">Kontakt</a></td>
+	<td><a href="tunniplaan.html">Tunniplaan</a></td>
+	<td><a href="opilased.html">Õpilased</a></td>
+	<td><a href="opetajad.html">Õpetajad</a></td>
+	</tr> -->
+	<tr>
+		<td><a href="uudised.php">Uudised</a></td>
+	</tr>
+	<tr>
+		<td><a href="kontakt.php">Kontakt</a></td>
+	</tr>
+	<tr>
+		<td><a href="tunniplaan.php">Tunniplaan</a></td>
+	</tr>
+	<tr>
+		<td><a href="opilased.php">Õpilased</a></td>
+	</tr>
+	<tr>
+		<td><a href="opetajad.php">Õpetajad</a></td>
+	</tr>
+<!--2.stroka-->
+	<tr>
+	<td colspan="5" class="suur">
+    <iframe width="560" height="315" src="http://www.youtube.com/embed/IjbQnY4j4cw" frameborder="0" allowfullscreen></iframe>
+	</td>
+	</tr>
+
+</table>
+</div>
+</body>
+</html>
