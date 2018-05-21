572588a7771328dccbea2cbb8303e9803b076f12
Nikita Rezvan <nikitarezvan@gmail.com>
2018-05-21 10:40:11 +0300

    MERKULOVA: Completed all tasks

---
+++ opetajad.php
@@ -0,0 +1,51 @@
+﻿<?php 
+echo ("<div style='position: absolute; right: 50; top: 10; font-size: 20px; background-color: orange; padding: 2px 5px 2px 5px; border: 2px solid black;'>");
+echo date("H:i");
+echo ("</div>");
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Kana KOOL</title>
+<link rel="stylesheet" type="text/css" href="kukestyle.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+</head>
+<body>
+<h1>Kana KOOL</h1>
+<div id="logo">
+<img src="petuh.png">
+</div>
+<div id="tabel">
+<table>
+	<tr>
+	<td><a href="uudised.html">Uudised</a></td>
+	<td><a href="kontakt.html">Kontakt</a></td>
+	<td><a href="tunniplaan.html">Tunniplaan</a></td>
+	<td><a href="opilased.html">Õpilased</a></td>
+	<td><a href="opetajad.html">Õpetajad</a></td>
+	</tr>
+	<tr>
+	<td colspan="5" class="suur">
+	<ol>
+	<li>valik 1
+	<li>valik 2
+	<li>valik 3
+	<li>valik 4
+	<li>valik 5
+	<li>valik 6
+	<li>valik 7
+	<li>valik 8
+	</ol>
+	</td>
+	</tr>
+
+</table>
+</div>
+</body>
+</html>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
---
+++ uudised.php
@@ -0,0 +1,46 @@
+﻿<?php 
+echo ("<div style='position: absolute; right: 50; top: 10; font-size: 20px; background-color: orange; padding: 2px 5px 2px 5px; border: 2px solid black;'>");
+echo date("H:i");
+echo ("</div>");
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Kana KOOL</title>
+<link rel="stylesheet" type="text/css" href="kukestyle.css">
+</head>
+<body>
+<h1>Kana KOOL</h1>
+<div id="logo">
+<img src="petuh.png">
+</div>
+<div id="tabel">
+<table>
+	<tr>
+	<td><a href="uudised.html">Uudised</a></td>
+	<td><a href="kontakt.html">Kontakt</a></td>
+	<td><a href="tunniplaan.html">Tunniplaan</a></td>
+	<td><a href="opilased.html">Õpilased</a></td>
+	<td><a href="opetajad.html">Õpetajad</a></td>
+	</tr>
+	<tr>
+	<td colspan="5" class="suur">
+<table class="vaike">
+<tr>
+	<td class="vaike"><img src="kukk1.jpg"></td>
+	<td class="vaike"><img src="kukk2.jpg"></td>
+</tr>
+<tr>
+	<td class="vaike"><img src="kukk3.jpg"></td>
+	<td class="vaike"><img src="kukk4.jpg"></td>
+</tr>
+</table>
+	
+	</td>
+	</tr>
+
+</table>
+</div>
+</body>
+</html>
